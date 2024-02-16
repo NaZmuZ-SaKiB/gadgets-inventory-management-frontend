b1959ad8d9b8738522c35068fdd701e0e8c286f8
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-02-16 12:29:34 +0600

    added user role type

--- src/types/user.interface.ts
+++ src/types/user.interface.ts
@@ -1,7 +1,11 @@
+export type TUserRole = "user" | "manager" | "admin";
+
 export type TUser = {
   _id: string;
   name: string;
   email: string;
   password: string;
-  role: string;
+  role: TUserRole;
+  createdAt: string;
+  updatedAt: string;
 };
